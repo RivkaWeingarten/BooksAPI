87bfee0afe2cdd1cca5712dcd9a3fc1d1a1c5647
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-07-27 14:34:54 -0400

    Books API Homework

--- models/books.js
+++ models/books.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
